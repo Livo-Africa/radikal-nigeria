4ab4c2b41c5e4963d183191d3376e54e10d9f81c
LivoAfrica <livoafrica@gmail.com>
2026-02-21 05:09:07 +0000

    new robot.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: '*',
             allow: '/',
-            disallow: ['/api/', '/admin/', '/orders/', '/lib/', '/hooks/', '/utils/'], // Add any paths you want to hide from search engines
         },
         sitemap: 'https://radikalcreatech.com/sitemap.xml',
     };
